e45a23ca807c43ba573b383899f5bf7c9ad6c0ad
Merlin Moelter <joebau.0815@googlemail.com>
2020-11-19 11:10:08 +0100

    Add dynamic method creation to command class

--- app/lib/Command.js
+++ app/lib/Command.js
@@ -1,42 +1,25 @@
+const properties = [
+    "name",
+    "group",
+    "description",
+    "usage",
+    "alias",
+    "permissions"
+]
+
 class Command {
     constructor(run) {
         this.run = run
-        this.name = null
-        this.group = null
-        this.description = null
-        this.usage = null
-        this.alias = null
-        this.permissions = null
-    }
 
-    setName(name) {
-        this.name = name
-        return this
-    }
+        // Generate chainable setters for properties
+        properties.forEach(prop => {
+            this[prop] = null
 
-    setGroup(group) {
-        this.group = group
-        return this
-    }
-
-    setDescription(description) {
-        this.description = description
-        return this
-    }
-
-    setUsage(usage) {
-        this.usage = usage
-        return this
-    }
-
-    setAlias(alias) {
-        this.alias = alias
-        return this
-    }
-
-    setPermissions(permissions) {
-        this.permissions = permissions
-        return this
+            this["set" + prop.charAt(0).toUpperCase() + prop.slice(1)] = function(value) {
+                this[prop] = value
+                return this
+            }
+        })
     }
 }
 
