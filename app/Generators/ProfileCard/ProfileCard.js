e7bc3959b112eec19b8bace4c586a45f8ad99f45
Merlin Moelter <joebau.0815@googlemail.com>
2020-11-23 13:39:04 +0100

    Change background color of profile cards

--- app/Generators/ProfileCard/ProfileCard.js
+++ app/Generators/ProfileCard/ProfileCard.js
@@ -14,7 +14,7 @@ class ProfileCard {
 
         this.theme = {
             spacing: 16,
-            backgroundColor: "#33373E",
+            backgroundColor: "#202225",
             palette: {
                 primaryVariant: "#f8f9fa",
                 secondary: "rgba(255, 255, 255, .7)",
