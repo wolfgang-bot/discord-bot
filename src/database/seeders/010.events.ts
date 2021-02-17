6efd9255f21eefdf1b2aef96cec677c6021e9b70
Merlin Moelter <joebau.0815@googlemail.com>
2021-02-17 21:01:50 +0100

    Improve events seeder

--- src/database/seeders/010.events.ts
+++ src/database/seeders/010.events.ts
@@ -1,28 +1,35 @@
 import Event, { EVENT_TYPES } from "../../models/Event"
 import { Seeder, ProgressCallback } from "../index"
 
-const GENERATE_DATA_FOR_DAYS = 14
+const GENERATE_DATA_FOR_DAYS = 30
 const GUILD_ID = "786167187030540309"
-const MILLISECONDS_PER_DAY = 24 * 60 * 60 * 1000
+
+const SKIP_DAY_PROBABILITY = .1
 
 const INITIAL_MEMBER_COUNT = 1000
-const MIN_AMOUNT_OF_MEMBER_EVENTS = 100
-const MAX_AMOUNT_OF_MEMBER_EVENTS = 200
-const MEMBER_JOIN_LEAVE_RATIO = .8
+const MIN_AMOUNT_OF_MEMBER_EVENTS_PER_DAY = 100
+const MAX_AMOUNT_OF_MEMBER_EVENTS_PER_DAY = 300
+const MEMBER_JOIN_LEAVE_RATIO = .7
+const MEMBER_DROP_PROBABILITY = .1
 
-const MIN_AMOUNT_OF_MESSAGES = 100
-const MAX_AMOUNT_OF_MESSAGES = 300
+const MIN_AMOUNT_OF_MESSAGES_PER_DAY = 100
+const MAX_AMOUNT_OF_MESSAGES_PER_DAY = 300
+
+const RANDOM_TIMESTAMPS_SPREAD_IN_MILLISECONDS = 1e4
+
+const MILLISECONDS_PER_DAY = 24 * 60 * 60 * 1000
 
 function random(min: number, max: number) {
     return Math.floor(Math.random() * (max - min) + min)
 }
 
 function createRandomTimestampNDaysAgo(days: number) {
-    return Date.now() - days * MILLISECONDS_PER_DAY + random(0, 1e4)
+    const randomOffset = random(0, RANDOM_TIMESTAMPS_SPREAD_IN_MILLISECONDS)
+    return Date.now() - days * MILLISECONDS_PER_DAY + randomOffset
 }
 
 function shouldGenerateData() {
-    return Math.random() > .2
+    return Math.random() > SKIP_DAY_PROBABILITY
 }
 
 function sum(numbers: number[]) {
@@ -36,9 +43,10 @@ async function generateMemberEvents(callback: ProgressCallback) {
     const amountsOfEntries = Array(GENERATE_DATA_FOR_DAYS)
         .fill(0)
         .map(() => (
-            shouldGenerateData() ?
-            random(MIN_AMOUNT_OF_MEMBER_EVENTS, MAX_AMOUNT_OF_MEMBER_EVENTS) :
-            0
+            shouldGenerateData() ? random(
+                MIN_AMOUNT_OF_MEMBER_EVENTS_PER_DAY,
+                MAX_AMOUNT_OF_MEMBER_EVENTS_PER_DAY
+            ) : 0
         ))
 
     const totalAmountOfEntries = sum(amountsOfEntries)
@@ -52,7 +60,7 @@ async function generateMemberEvents(callback: ProgressCallback) {
     let memberCount = INITIAL_MEMBER_COUNT
     
     for (let i = amountsOfEntries.length - 1; i >= 0; i--) {
-        const randomFactor = Math.random() > .8 ? -1 : 1
+        const randomFactor = Math.random() < MEMBER_DROP_PROBABILITY ? -1 : 1
         
         for (let j = 0; j < amountsOfEntries[i]; j++) {
             const type = Math.random() >= MEMBER_JOIN_LEAVE_RATIO * randomFactor ?
@@ -87,11 +95,12 @@ async function generateMessageEvents(callback: ProgressCallback) {
     const amountsOfEntries = Array(GENERATE_DATA_FOR_DAYS)
         .fill(0)
         .map(() => (
-            shouldGenerateData() ?
-            random(MIN_AMOUNT_OF_MESSAGES, MAX_AMOUNT_OF_MESSAGES) :
-            0
+            shouldGenerateData() ? random(
+                MIN_AMOUNT_OF_MESSAGES_PER_DAY,
+                MAX_AMOUNT_OF_MESSAGES_PER_DAY
+            ) : 0
         ))
-
+            
     const totalAmountOfEntries = sum(amountsOfEntries)
 
     callback({
