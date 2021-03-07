0179df74f6f6000636f109fe9adcaf8803763e29
Merlin Moelter <joebau.0815@googlemail.com>
2021-03-07 13:46:08 +0100

    Fix SVGComponent children default value

--- src/lib/SVGComponent.ts
+++ src/lib/SVGComponent.ts
@@ -1,7 +1,7 @@
 export default abstract class SVGComponent {
     children: {
         [label: string]: SVGComponent
-    }
+    } = {}
 
     abstract getWidth(): number
     abstract getHeight(): number
