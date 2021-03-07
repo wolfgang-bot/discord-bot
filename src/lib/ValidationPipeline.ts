6caed2bb0fb47734ae0b23a3cc3aa5fd54695e3a
Merlin Moelter <joebau.0815@googlemail.com>
2021-03-07 23:02:43 +0100

    Implement validation pipeline

---
+++ src/lib/ValidationPipeline.ts
@@ -0,0 +1,65 @@
+import Discord from "discord.js"
+import User from "../models/User"
+import Guild from "../models/Guild"
+
+export abstract class Validator {
+    abstract run(client: Discord.Client, input: object): Promise<object>
+
+    constructor(protected error: any) {}
+}
+
+export default class ValidationPipeline {
+    constructor(
+        private client: Discord.Client,
+        private validators: Validator[]
+    ) { }
+
+    async validate(input: object): Promise<any> {
+        const args = { ...input }
+
+        for (let validator of this.validators) {
+            try {
+                Object.assign(args, await validator.run(this.client, args))
+            } catch (error) {
+                return error
+            }
+        }
+    }
+}
+
+export class GuildExistsValidator extends Validator {
+    async run(client: Discord.Client, { guildId }: { guildId: string }) {
+        const guild = await Guild.findBy("id", guildId) as Guild
+
+        if (!guild) {
+            throw this.error
+        }
+
+        return { guild }
+    }
+}
+
+export class GuildAvailableValidator extends Validator {
+    async run(client: Discord.Client, { guild }: { guild: Guild }) {
+        await guild.fetchDiscordGuild(client)
+
+        if (!guild.discordGuild.available) {
+            throw this.error
+        }
+
+        return { guild }
+    }
+}
+
+export class GuildAdminValidator extends Validator {
+    async run(client: Discord.Client, { guild, user }: {
+        guild: Guild,
+        user: User
+    }) {
+        if (!await user.isAdmin(guild)) {
+            throw this.error
+        }
+
+        return { guild, user }
+    }
+}
