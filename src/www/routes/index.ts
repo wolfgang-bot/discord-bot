27309d2bcc6a45d155ac730a92c0d95dd0305c63
Merlin Moelter <joebau.0815@googlemail.com>
2021-03-07 17:20:06 +0100

    Fix frontend proxy

--- src/www/routes/index.ts
+++ src/www/routes/index.ts
@@ -10,7 +10,7 @@ rootRouter.use("/storage", storage)
 
 // Proxy frontend server
 rootRouter.use("/", createProxyMiddleware({
-    target: `http://localhost:/${process.env.FRONTEND_PORT}`,
+    target: `http://localhost:${process.env.FRONTEND_PORT}/`,
     // ws: true -> Crashes the dev-server when reloading via nodemon in combination with socket.io websocket
 }))
 
