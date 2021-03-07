7f764ae22ce0d6c71d2be59aba0ae36cbbd131a2
Merlin Moelter <joebau.0815@googlemail.com>
2021-03-07 12:11:26 +0100

    Remove module commands

--- src/modules/main/commands/index.ts
+++ src/modules/main/commands/index.ts
@@ -1,12 +1,10 @@
 import CommandGroup from "../../../lib/CommandGroup"
 import HelpCommand from "./help"
-import ModuleCommand from "./modules"
 
 export default class RootCommandGroup extends CommandGroup {
     constructor() {
         const commands = [
-            new HelpCommand(),
-            new ModuleCommand()
+            new HelpCommand()
         ]
 
         commands.forEach(command => command.module = "main")
