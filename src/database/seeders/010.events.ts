7ca90c1b4f9e54eef919ca6d140dd01c4aadd495
Merlin Moelter <joebau.0815@googlemail.com>
2021-03-14 15:17:59 +0100

    Add seeder for amount of guilds

--- src/database/seeders/010.events.ts
+++ src/database/seeders/010.events.ts
@@ -1,7 +1,7 @@
 import Event, { EVENT_TYPES } from "../../models/Event"
 import { Seeder, ProgressCallback } from "../index"
 
-const GENERATE_DATA_FOR_DAYS = 30
+const GENERATE_DATA_FOR_DAYS = 7
 const GUILD_ID = "786167187030540309"
 
 const SKIP_DAY_PROBABILITY = .1
@@ -9,6 +9,11 @@ const SKIP_DAY_PROBABILITY = .1
 const MILLISECONDS_PER_MINUTE = 60 * 1000
 const MILLISECONDS_PER_HOUR = MILLISECONDS_PER_MINUTE * 60
 const MILLISECONDS_PER_DAY = MILLISECONDS_PER_HOUR * 24
+
+const MIN_AMOUNT_OF_GUILDS_PER_DAY = 10
+const MAX_AMOUNT_OF_GUILDS_PER_DAY = 50
+const GUILD_ADD_REMOVE_RATIO = .8
+const GUILD_DROP_PROBABILITY = .1
 
 const INITIAL_MEMBER_COUNT = 1000
 const MIN_AMOUNT_OF_MEMBER_EVENTS_PER_DAY = 100
@@ -50,6 +55,63 @@ function sum(numbers: number[]) {
 }
 
 /**
+ * Generate guild add / remove events
+ */
+async function generateGuildEvents(callback: ProgressCallback) {
+    const amountsOfEntries = randomAmountOfEntries(
+        MIN_AMOUNT_OF_GUILDS_PER_DAY,
+        MAX_AMOUNT_OF_GUILDS_PER_DAY
+    )
+
+    const totalAmountOfEntries = sum(amountsOfEntries)
+
+    callback({
+        type: "init",
+        key: "Guilds",
+        data: totalAmountOfEntries
+    })
+
+    let guildCount = 0
+
+    for (let i = amountsOfEntries.length - 1; i >= 0; i--) {
+        const randomFactor = Math.random() < GUILD_DROP_PROBABILITY ? -1 : 1
+
+        for (let j = 0; j < amountsOfEntries[i]; j++) {
+            const type = Math.random() >= GUILD_ADD_REMOVE_RATIO * randomFactor ?
+                EVENT_TYPES.GUILD_ADD :
+                EVENT_TYPES.GUILD_REMOVE
+
+            const newGuildCount = type === EVENT_TYPES.GUILD_ADD ?
+                guildCount-- :
+                guildCount++
+
+            if (newGuildCount < 0) {
+                guildCount = 0
+                callback({
+                    type: "tick",
+                    key: "Guilds"
+                })
+                continue
+            }
+
+            await new Event({
+                type,
+                timestamp: createRandomTimestampNDaysAgo(i),
+                guild_id: "123",
+                meta: {
+                    guildCount: newGuildCount
+                }
+            }).store()
+
+            callback({
+                type: "tick",
+                key: "Guilds"
+            })
+        }
+    }
+}
+
+/**
  * Generate guild member add / remove events
  */
 async function generateMemberEvents(callback: ProgressCallback) {
@@ -79,6 +141,15 @@ async function generateMemberEvents(callback: ProgressCallback) {
             const newMemberCount = type === EVENT_TYPES.GUILD_MEMBER_REMOVE ?
                 memberCount-- :
                 memberCount++
+
+            if (newMemberCount < 0) {
+                memberCount = 0
+                callback({
+                    type: "tick",
+                    key: "Guilds"
+                })
+                continue
+            }
 
             await new Event({
                 type,
@@ -176,6 +247,7 @@ const seeder: Seeder = {
 
     run: async (callback: ProgressCallback) => {
         await Promise.all([
+            generateGuildEvents(callback),
             generateMemberEvents(callback),
             generateMessageEvents(callback),
             generateVoiceEvents(callback)
