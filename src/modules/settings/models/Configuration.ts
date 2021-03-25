6d0943fb4f1076c16ffe573f7e6a936ca6e03eaa
Merlin Moelter <joebau.0815@googlemail.com>
2021-03-25 16:54:44 +0100

    Limit command-prefix length

--- src/modules/settings/models/Configuration.ts
+++ src/modules/settings/models/Configuration.ts
@@ -1,6 +1,8 @@
 import DefaultConfig from "../../../lib/Configuration"
 import { HEX_COLOR_REGEX } from "../../../lib/constraints"
 
+const COMMAND_PREFIX_MAX_LENGTH = 8
+
 type ConfigProps = {
     commandPrefix: string,
     locale: string,
@@ -15,6 +17,10 @@ export default class Configuration extends DefaultConfig implements ConfigProps 
     constructor(props: ConfigProps) {
         super(props)
         
+        if (this.commandPrefix.length > COMMAND_PREFIX_MAX_LENGTH) {
+            throw `'Command Prefix' cannot be longer than ${COMMAND_PREFIX_MAX_LENGTH} characters`
+        }
+
         if (!HEX_COLOR_REGEX.test(this.colorPrimary)) {
             throw "'Color Primary' must be a valid hexadecimal color"
         }
