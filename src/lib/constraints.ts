4b3b719f8ae2749950b94e47eef42f22f772fece
Tracer1337 <merlin.moelter@gmail.com>
2021-04-26 21:06:25 +0200

    Add constraint utilities

--- src/lib/constraints.ts
+++ src/lib/constraints.ts
@@ -1,13 +1,59 @@
 import emojiRegex from "emoji-regex/RGI_Emoji"
 
+const CHANNEL_NAME_MIN_LENGTH = 1
+const CHANNEL_NAME_MAX_LENGTH = 50
+
+export type Constraint<T> = {
+    constraints: string,
+    verifyConstraints: (value: T) => boolean
+}
+
+export function useConstraint<
+    TProps extends Record<string, any>,
+    TValue extends TProps[keyof TProps]
+>(key: keyof TProps, constraint: Constraint<TValue>) {
+    return {
+        key,
+        validate: (props: TProps) => constraint.verifyConstraints(props[key]),
+        message: constraint.constraints
+    }
+}
+
+export function minMaxConstraint<T = number>(options: {
+    min: number,
+    max: number,
+    subjectName?: string,
+    getNumericValue?: (value: T) => number
+}): Constraint<T> {
+    const getNumericValue = options.getNumericValue || ((value: T) => value)
+
+    let constraints = ""
+    constraints += options.subjectName ? `${options.subjectName} must` : "Must"
+    constraints += ` be between ${options.min} and ${options.max}`
+
+    return {
+        constraints,
+        verifyConstraints: (value: T) =>
+            getNumericValue(value) >= options.min &&
+            getNumericValue(value) <= options.max
+    }
+}
+
 // Match hex colors
 export const HEX_COLOR_REGEX = /^#([0-9a-fA-F]{3}){1,2}$/
 // Match hex colors and discord color names
 export const COLOR_REGEX = /^((#([0-9a-fA-F]{3}){1,2})|(DEFAULT|WHITE|AQUA|GREEN|BLUE|YELLOW|PURPLE|LUMINOUS_VIVID_PINK|GOLD|ORANGE|RED|GREY|DARKER_GREY|NAVY|DARK_AQUA|DARK_GREEN|DARK_BLUE|DARK_PURPLE|DARK_VIVID_PINK|DARK_GOLD|DARK_ORANGE|DARK_RED|DARK_GREY|LIGHT_GREY|DARK_NAVY|BLURPLE|GREYPLE|DARK_BUT_NOT_BLACK|NOT_QUITE_BLACK|RANDOM))$/
 
-export const emojiConstraint = {
+export const emojiConstraint: Constraint<string> = {
     constraints: "Must be one unicode emoji",
-    verifyConstraints: (value: string) =>
+    verifyConstraints: (value) =>
         emojiRegex().test(value) &&
         value.match(emojiRegex()).length === 1
 }
+
+export const channelNameConstraint = minMaxConstraint({
+    min: CHANNEL_NAME_MIN_LENGTH,
+    max: CHANNEL_NAME_MAX_LENGTH,
+    subjectName: "Length",
+    getNumericValue: (value: string) => value.length
+})
