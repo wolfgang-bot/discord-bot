c6e2ee704fdcb4c4491cc31f1c5d8d22b8a02177
Tracer1337 <merlin.moelter@gmail.com>
2021-05-19 20:28:36 +0200

    Remove trailing slash from dashboard url

--- src/modules/main/embeds/DashboardEmbed.ts
+++ src/modules/main/embeds/DashboardEmbed.ts
@@ -7,7 +7,7 @@ export default class DashboardEmbed extends BaseEmbed {
     constructor(settings: SettingsConfig, guild: Discord.Guild) {
         super(settings)
 
-        const url = makeURL(`/dashboard/guild/${guild.id}/`)
+        const url = makeURL(`/dashboard/guild/${guild.id}`)
 
         this.setTitle("Dashboard")
             .setDescription(`[Open in browser](${url})`)
